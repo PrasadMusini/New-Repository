7d3c210c01af4aa35a6aff9e41a92a03208a52dd
Anil519 <anil.chukka@nexgile.com>
2023-05-01 16:38:07 +0530

    header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path:'h',
+   component:HeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
